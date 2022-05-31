a97b4e0bc72d740988097dbfa2dee283fac690d0
plz-no-anr <psgxxx@naver.com>
2022-05-31 13:23:58 +0900

    Modify Splash

--- app/src/main/java/com/psg/lottoapp/view/splash/SplashActivity.kt
+++ app/src/main/java/com/psg/lottoapp/view/splash/SplashActivity.kt
@@ -2,18 +2,20 @@ package com.psg.lottoapp.view.splash
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.psg.lottoapp.R
 import com.psg.lottoapp.data.model.LottoEntity
 import com.psg.lottoapp.databinding.ActivitySplashBinding
 import com.psg.lottoapp.view.base.BaseActivity
 import com.psg.lottoapp.view.main.MainActivity
-import org.koin.android.ext.android.inject
+import dagger.hilt.android.AndroidEntryPoint
 import java.text.SimpleDateFormat
 import java.util.*
 
+@AndroidEntryPoint
 class SplashActivity : BaseActivity<ActivitySplashBinding,SplashViewModel>(R.layout.activity_splash) {
     override val TAG: String = MainActivity::class.java.simpleName
-    override val viewModel: SplashViewModel by inject()
+    override val viewModel: SplashViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,25 +26,25 @@ class SplashActivity : BaseActivity<ActivitySplashBinding,SplashViewModel>(R.lay
         val intent = Intent(this,MainActivity::class.java)
         intent.flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
 
-        viewModel.lottoEntity.observe(this,{
-            if (it != null){
+        viewModel.lottoEntity.observe(this) {
+            if (it != null) {
                 val sf = SimpleDateFormat("yyyy-MM-dd 00:00:00")
                 val date = sf.parse("${it.drwNoDate} 00:00:00")
                 val today = Calendar.getInstance()
                 val calDate = (today.time.time - date.time) / (60 * 60 * 24 * 1000)
                 println("저장된 회차:${it.drwNo}")
-                if (calDate > 7){
+                if (calDate > 7) {
                     println("최신회차가 아님")
                     println("날짜차이:${(calDate / 7)}주지남}")
                     viewModel.searchLotto((it.drwNo + (calDate / 7).toInt()))
-                    viewModel.lottoNum.observe(this,{ num ->
-                        if (num != null){
+                    viewModel.lottoNum.observe(this) { num ->
+                        if (num != null) {
                             viewModel.deleteLotto()
-                            viewModel.insertLotto(LottoEntity(num.drwNo,num.drwNoDate))
+                            viewModel.insertLotto(LottoEntity(num.drwNo, num.drwNoDate))
                             startActivity(intent)
                             finish()
                         }
-                    })
+                    }
 
                 } else {
                     println("최신회차")
@@ -58,17 +60,17 @@ class SplashActivity : BaseActivity<ActivitySplashBinding,SplashViewModel>(R.lay
                 val calDate = (today.time.time - date.time) / (60 * 60 * 24 * 1000)
                 println("날짜차이:${(calDate / 7)}주지남")
 
-                viewModel.searchLotto(1000+(calDate / 7).toInt())
-                viewModel.lottoNum.observe(this,{ num ->
-                    if (num != null){
-                        viewModel.insertLotto(LottoEntity(num.drwNo,num.drwNoDate))
+                viewModel.searchLotto(1000 + (calDate / 7).toInt())
+                viewModel.lottoNum.observe(this) { num ->
+                    if (num != null) {
+                        viewModel.insertLotto(LottoEntity(num.drwNo, num.drwNoDate))
                         startActivity(intent)
                         finish()
                     }
-                })
+                }
 
             }
-        })
+        }
     }
 
 
--- app/src/main/java/com/psg/lottoapp/view/splash/SplashViewModel.kt
+++ app/src/main/java/com/psg/lottoapp/view/splash/SplashViewModel.kt
@@ -3,26 +3,31 @@ package com.psg.lottoapp.view.splash
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.psg.lottoapp.data.model.Lotto
 import com.psg.lottoapp.data.model.LottoEntity
-import com.psg.lottoapp.data.model.LottoNum
+import com.psg.lottoapp.data.model.LottoResponse
 import com.psg.lottoapp.data.repository.AppRepository
+import com.psg.lottoapp.util.responseToLotto
 import com.psg.lottoapp.view.base.BaseViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import javax.inject.Inject
 
-class SplashViewModel(private val repository: AppRepository): BaseViewModel() {
+@HiltViewModel
+class SplashViewModel @Inject constructor(private val repository: AppRepository): BaseViewModel() {
     val lottoEntity: LiveData<LottoEntity> get() = repository.getLottoNum()
-    val lottoNum: LiveData<LottoNum> get() = _lottoNum
-    private var _lottoNum = MutableLiveData<LottoNum>()
+    val lottoNum: LiveData<Lotto> get() = _lottoNum
+    private var _lottoNum = MutableLiveData<Lotto>()
 
     fun updateLotto(lottoEntity: LottoEntity) = CoroutineScope(Dispatchers.IO).launch { repository.updateLotto(lottoEntity) }
     fun insertLotto(lottoEntity: LottoEntity) = CoroutineScope(Dispatchers.IO).launch { repository.insertLotto(lottoEntity) }
     fun deleteLotto() = CoroutineScope(Dispatchers.IO).launch { repository.deleteLotto() }
 
     fun searchLotto(drwNo:Int){
-        var res = LottoNum("","",0,0,0,0,0,0,0,0,0,0,0,0)
+        var res = LottoResponse("","",0,0,0,0,0,0,0,0,0,0,0,0)
         viewModelScope.launch {
             withContext(Dispatchers.Default){
                 val body = repository.searchLotto(drwNo).body()
@@ -33,7 +38,7 @@ class SplashViewModel(private val repository: AppRepository): BaseViewModel() {
                     res = body
                 }
             }
-            _lottoNum.value = res
+            _lottoNum.value = responseToLotto(res)
 
         }
     }
